1582b444cf92589bd31747a1754ecff6f14a93f7
fauky <fmented@gmail.com>
2022-04-11 01:18:29 +0700

    env thingy

---
+++ src/lib/secrets.js
@@ -0,0 +1,8 @@
+export const PASSWORD = import.meta.env.VITE_PASSWORD
+export const USERNAME = import.meta.env.VITE_USERNAME
+export const JWT_SECRET = import.meta.env.VITE_JWT_SECRET
+export const CLUSTER_URL = import.meta.env.VITE_CLUSTER_URL
+export const DB_NAME = import.meta.env.VITE_DB_NAME
+export const PRIVATE_KEY = import.meta.env.VITE_PRIVATE_KEY
+export const SUBJECT = import.meta.env.VITE_SUBJECT
+export const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY
